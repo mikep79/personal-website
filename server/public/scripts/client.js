9488f1b971801106a08b9513e170750714f41542
Mike Pettman <mp_palmatum@yahoo.com>
2018-01-01 12:12:26 -0600

    added template for separate project pages

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,7 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngAnimate']);
 
 /// Routes ///
-myApp.config(function($routeProvider, $locationProvider) {
+myApp.config(function ($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
 
   $routeProvider
@@ -13,15 +13,24 @@ myApp.config(function($routeProvider, $locationProvider) {
       templateUrl: '/views/repos.html',
       controller: 'ReposController as RC'
     })
-    .when('/projects', {
-      templateUrl: '/views/projects.html',
-      controller: 'ProjectsController as PC'
-    })
     .when('/blog', {
       templateUrl: '/views/blog.html'
     })
     .when('/about', {
       templateUrl: '/views/about.html'
+    })
+    .when('/projects', {
+      templateUrl: '/views/projects.html',
+      controller: 'ProjectsController as PC'
+    })
+    .when('/projects/jump', {
+      templateUrl: 'views/projects/jump.html'
+    })
+    .when('/projects/grow-north', {
+      templateUrl: 'views/projects/grow-north.html'
+    })
+    .when('/projects/market-mania', {
+      templateUrl: 'views/projects/market-mania.html'
     })
     .otherwise({
       redirectTo: '/'
